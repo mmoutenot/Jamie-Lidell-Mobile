0771fe1b4a83595101f8ee5cea96497b5b55f41c
Marshall <mmoutenot@gmail.com>
2013-03-07 12:41:55 -0500

    pushed to the app store

--- JamieLidellMobile/JLViewController.h
+++ JamieLidellMobile/JLViewController.h
@@ -7,7 +7,36 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "FlashController.h"
+
+typedef enum lightState {
+  ls_off      = 0,
+  ls_on       = 1,
+  ls_strobe_1 = 2,
+  ls_strobe_2 = 3,
+  ls_strobe_3 = 4,
+  ls_strobe_4 = 5,
+} LightState;
 
 @interface JLViewController : UIViewController
+
+@property (retain, nonatomic) FlashController *fc;
+@property (nonatomic, retain) NSTimer *strobeTimer;
+@property (nonatomic, assign) LightState *nextLightState;
+@property (nonatomic, assign) BOOL lightOn;
+@property (nonatomic, assign) BOOL strobeActivated;
+@property (nonatomic, assign) BOOL strobeFlashOn;
+@property (nonatomic, assign) float strobeRate;
+
+@property (strong, nonatomic) IBOutlet UIButton *jamieButton;
+@property (strong, nonatomic) UIImage *buttonImage;
+@property (weak, nonatomic) IBOutlet UILabel *flasher_title;
+
+@property (strong, nonatomic) NSTimer *hueTimer;
+@property (strong, nonatomic) NSNumber *currentHue;
+
+- (IBAction)buttonDown:(id)sender;
+- (IBAction)websiteButtonClick:(id)sender;
+- (IBAction)explainButtonClick:(id)sender;
 
 @end
--- JamieLidellMobile/JLViewController.m
+++ JamieLidellMobile/JLViewController.m
@@ -7,6 +7,12 @@
 //
 
 #import "JLViewController.h"
+#import <QuartzCore/QuartzCore.h>
+
+#define STROBE_RATE_1 0.56f
+#define STROBE_RATE_2 0.28f
+#define STROBE_RATE_3 0.14f
+#define STROBE_RATE_4 0.07f
 
 @interface JLViewController ()
 
@@ -16,14 +22,122 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+  [super viewDidLoad];
+  
+  _fc = [[FlashController alloc] init];
+  _nextLightState = ls_on;
+  _lightOn = NO;
+  _strobeActivated = NO;
+  _strobeFlashOn = NO;
+  _strobeRate = STROBE_RATE_1;
+  
+  
+  _buttonImage = [_jamieButton imageForState:UIControlStateNormal];
+  _currentHue = [NSNumber numberWithFloat:0.2f];
+}
+
+- (void)strobeTimerCallback:(id)sender {
+	if (_strobeActivated) {
+		_lightOn = !_lightOn;
+		[self startStopStrobe:_lightOn];
+	} else {
+		[self startStopStrobe:NO];
+	}
+}
+
+- (void)startStopStrobe:(BOOL)strobeOn {
+	if (strobeOn && _strobeActivated) {
+		[_fc toggleStrobe:YES];
+	} else {
+		[_fc toggleStrobe:NO];
+	}
+}
+
+- (void)performHueShiftOnButtonImage{
+  CIImage *inputImage = [[CIImage alloc] initWithImage:_buttonImage];
+  CIContext *context = [CIContext contextWithOptions:nil];
+  
+  CIFilter *hueAdjust = [CIFilter filterWithName:@"CIHueAdjust"];
+  [hueAdjust setValue: inputImage forKey: @"inputImage"];
+  [hueAdjust setValue: _currentHue forKey: @"inputAngle"];
+  CIImage *outputImage = [hueAdjust valueForKey: @"outputImage"];
+  
+  //  return resultImage;
+  CGImageRef cgimg = [context createCGImage:outputImage fromRect:[outputImage extent]];
+  UIImage *newImage = [UIImage imageWithCGImage:cgimg];
+  
+  [_jamieButton setImage:newImage forState:UIControlStateNormal];
+  CGImageRelease(cgimg);
+  _currentHue = [NSNumber numberWithFloat:_currentHue.floatValue + 0.4f];
+  
+  CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
+  CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
+  CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
+  
+  [_flasher_title setTextColor:[UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1.f]];
 }
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)buttonDown:(id)sender {
+  [_hueTimer invalidate];
+  [_strobeTimer invalidate];
+  switch ((int)_nextLightState) {
+    case ls_off:
+      _nextLightState = ls_on;
+      _strobeActivated = NO;
+      break;
+    case ls_on:
+      _hueTimer = [NSTimer scheduledTimerWithTimeInterval:0.2f target:self selector:@selector(performHueShiftOnButtonImage) userInfo:nil repeats:YES];
+      _nextLightState = ls_strobe_1;
+      _strobeActivated = YES;
+      break;
+    case ls_strobe_1:
+      _strobeRate = STROBE_RATE_1;
+      _hueTimer = [NSTimer scheduledTimerWithTimeInterval:_strobeRate/2 target:self selector:@selector(performHueShiftOnButtonImage) userInfo:nil repeats:YES];
+      _strobeTimer = [NSTimer scheduledTimerWithTimeInterval:_strobeRate target:self selector:@selector(strobeTimerCallback:) userInfo:nil repeats:YES];
+      _nextLightState = ls_strobe_2;
+      break;
+    case ls_strobe_2:
+      _strobeRate = STROBE_RATE_2;
+      _hueTimer = [NSTimer scheduledTimerWithTimeInterval:_strobeRate/2 target:self selector:@selector(performHueShiftOnButtonImage) userInfo:nil repeats:YES];
+      _strobeTimer = [NSTimer scheduledTimerWithTimeInterval:_strobeRate target:self selector:@selector(strobeTimerCallback:) userInfo:nil repeats:YES];
+      _nextLightState = ls_strobe_3;
+      break;
+    case ls_strobe_3:
+      _strobeRate = STROBE_RATE_3;
+      _hueTimer = [NSTimer scheduledTimerWithTimeInterval:_strobeRate/2 target:self selector:@selector(performHueShiftOnButtonImage) userInfo:nil repeats:YES];
+      _strobeTimer = [NSTimer scheduledTimerWithTimeInterval:_strobeRate target:self selector:@selector(strobeTimerCallback:) userInfo:nil repeats:YES];
+      _nextLightState = ls_strobe_4;
+      break;
+    case ls_strobe_4:
+      _strobeRate = STROBE_RATE_4;
+      _hueTimer = [NSTimer scheduledTimerWithTimeInterval:_strobeRate/2 target:self selector:@selector(performHueShiftOnButtonImage) userInfo:nil repeats:YES];
+      _strobeTimer = [NSTimer scheduledTimerWithTimeInterval:_strobeRate target:self selector:@selector(strobeTimerCallback:) userInfo:nil repeats:YES];
+      _nextLightState = ls_off;
+      break;
+    default:
+      break;
+  }
+	[self startStopStrobe:_strobeActivated];
+}
+
+-(IBAction)websiteButtonClick:(id)sender {
+  NSString* launchUrl = @"http://www.jamielidell.com/";
+  [[UIApplication sharedApplication] openURL:[NSURL URLWithString: launchUrl]];
+}
+
+-(IBAction)explainButtonClick:(id)sender {
+  NSString* launchUrl = @"http://snppi.com/jamie";
+  [[UIApplication sharedApplication] openURL:[NSURL URLWithString: launchUrl]];
+}
+
+- (void)viewDidUnload {
+  [self setFlasher_title:nil];
+  [super viewDidUnload];
+}
 @end
